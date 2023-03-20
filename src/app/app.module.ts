9e0431377fd24fe3a7a278f42126410aa52e73b6
Martin Rojas <110266070+SergioMartinRojas@users.noreply.github.com>
2023-03-19 21:41:14 -0300

    actualizamos luego de muchos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './componentes/hys/hys.component';
 import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -25,12 +29,15 @@ import { FooterComponent } from './componentes/footer/footer.component';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
